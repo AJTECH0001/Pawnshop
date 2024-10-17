a31f1dcbfb411b2eacc8b280701d32bd699810d1
amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-10-17 18:34:18 +0100

    connecting particle network

--- pawnshop-frontend/context/AuthContext.jsx
+++ pawnshop-frontend/context/AuthContext.jsx
@@ -4,6 +4,8 @@ import { useEthereum, useConnect, useAuthCore } from "@particle-network/auth-cor
 import { ArbitrumSepolia, chains } from "@particle-network/chains"
 import { AAWrapProvider, SmartAccount, SendTransactionMode } from "@particle-network/aa"
 import { createClient } from 'viem'
+import { ethers } from "ethers";
+
 
 
 const SocialoginAccount = createClient();
@@ -21,7 +23,34 @@ const AuthContext = ({ children }) => {
     }
   })
 
-  
+  const customProvider = new ethers.providers.Web3Provider(new AAWrapProvider(smartAccount, SendTransactionMode.Gasless), "any");
+  const [balance, setBalance] = useState(null);
+  const [address, setAddress] = useState(null);
+  const [signer, setSigner] = useState(null);
+
+
+  const { connect, disconnect, connected } = useConnect();
+  const { userInfo } = useAuthCore();
+
+  // fetch balanece
+  const fetchBalance = async () => {
+    const addressparticle = await smartAccount.provider.selectedAddress
+    setAddress(addressparticle)
+
+    const balanceResponseparticle = await customProvider.getBalance(
+      addressparticle
+    )
+    const balanceRes = ethers.utils.formatEthers(balanceResponseparticle)
+    setBalance(balanceRes)
+    const signers = customProvider?.getSigner();
+    setSigner(signers)
+  }
+
+  useEffect(() => {
+    if (userInfo) {
+      fetchBalance();
+    }
+  }, [userInfo, customProvider])
 
   return (
     <div>AuthContext</div>
--- pawnshop-frontend/src/pages/_app.tsx
+++ pawnshop-frontend/src/pages/_app.tsx
@@ -5,7 +5,8 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-
+import { ArbitrumSepolia } from '@particle-network/chains';
+import { AuthCoreContextProvider } from '@particle-network/auth-core-modal';
 import { config } from '../wagmi';
 
 const client = new QueryClient();
@@ -15,7 +16,28 @@ function MyApp({ Component, pageProps }: AppProps) {
     <WagmiProvider config={config}>
       <QueryClientProvider client={client}>
         <RainbowKitProvider>
-          <Component {...pageProps} />
+        <AuthCoreContextProvider
+      options={{
+        projectId: process.env.NEXT_PUBLIC_APP_PROJECT_ID || "",  
+        clientKey: process.env.NEXT_PUBLIC_APP_CLIENT_KEY || "",
+        appId: process.env.NEXT_PUBLIC_APP_APP_ID || "",// --
+        erc4337: {
+          // Optional
+          name: 'SIMPLE', // The smart account you intend to use
+          version: '1.0.0', // Leave as 1.0.0 unless you're using Biconomy V2
+        },
+        wallet: {
+          // Optional
+          visible: true, // Whether or not the embedded wallet modal is shown
+          customStyle: {
+            supportChains: [ArbitrumSepolia], // Locking the modal to Arbitrum Sepolia
+          },
+        },
+      }}
+    >
+      <Component {...pageProps} />
+       </AuthCoreContextProvider>
+          
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
