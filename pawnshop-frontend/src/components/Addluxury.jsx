279b334dd179825e070a3fb1e6261d13e64d3f43
amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-10-26 01:02:10 +0100

    update UI code

--- pawnshop-frontend/src/components/Addluxury.jsx
+++ pawnshop-frontend/src/components/Addluxury.jsx
@@ -2,25 +2,36 @@ import React, { useState } from "react";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Select } from "./ui/select";
+import { Button } from "./ui/button"
+// import { useAuth } from "../context/AuthContext";
 
 const Addluxury = () => {
   const [itemName, setItemName] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState(0);
+  const [image, setImage] = useState('')
+  const [watchType, setWatchType] = useState("");
+
+//   const { signerP } = useAuth()
+
 
   return (
     <div className=" flex justify-around items-center py-9">
       <form action="" className="space-y-4">
         <div className="w-[400px]">
-        <Label htmlFor="watchtype">Luxury Name</Label>
-          <Select>
+        <Label htmlFor="watchtype">Watch Type</Label>
+          <Select
+          id="watchtype"
+          value={watchType}
+          onChange={(e) => setWatchType(e.target.value)}
+          >
             <option value="option1">Rolex</option>
             <option value="option2">Hublut</option>
             <option value="option3">Frank Miller</option>
             <option value="option3">Omega</option>
           </Select>
 
-          <Label htmlFor="luxury">Luxury Name</Label>
+          <Label htmlFor="luxury">Model Name</Label>
           <Input
             id="luxury"
             placeholder="Luxury Name"
@@ -28,7 +39,15 @@ const Addluxury = () => {
             onChange={(e) => setItemName(e.target.value)}
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-          <Label htmlFor="description">Luxury Name</Label>
+          <Label htmlFor="image">Image URL</Label>
+          <Input
+            id="image"
+            placeholder="Recipet URL"
+            value={itemName}
+            onChange={(e) => setImage(e.target.value)}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <Label htmlFor="description">Condition</Label>
           <Input
             id="description"
             placeholder="Luxury Condition"
@@ -36,7 +55,7 @@ const Addluxury = () => {
             onChange={(e) => setDescription(e.target.value)}
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-          <Label htmlFor="price">Luxury Name</Label>
+          <Label htmlFor="price">Price</Label>
           <Input
             id="price"
             placeholder="Price"
@@ -44,6 +63,9 @@ const Addluxury = () => {
             onChange={(e) => setPrice(e.target.value)}
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
+          <Button type="submit" className="w-full mt-5 px-6 py-3 text-white bg-blue-500 rounded-md hover:bg-blue-600">
+            Add Luxury
+          </Button>
 
           
         </div>
---
+++ pawnshop-frontend/src/pages/luxurys.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ItemCard from '../components/ItemCard'
+const Luxurys = () => {
+  return (
+    <div>
+        <ItemCard />
+    </div>
+  )
+}
+
+export default Luxurys
